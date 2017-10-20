eb4fc102f2c83b093fe40244ae66dcaeadf26abb
song <xiaosong_play@163.com>
2017-10-20 15:00:33 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/leafweather/leafweather/MainActivity.java
+++ app/src/main/java/com/leafweather/leafweather/MainActivity.java
@@ -2,6 +2,10 @@ package com.leafweather.leafweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.tablemanager.Connector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Connector.getDatabase();
+            }
+        });
     }
 }
